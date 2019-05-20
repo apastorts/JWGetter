f1b0ccc1f822c8e8622b0359d7cee104f0db6183
Abel Pastor <abel.pastor@redhub.com.au>
2019-05-21 09:12:42 +1000

    Added TalkService to handle string stripping

--- app/Controllers/MidweekController.php
+++ app/Controllers/MidweekController.php
@@ -5,6 +5,7 @@ namespace Apastorts\JWGetter\Controllers;
 use PHPHtmlParser\Dom;
 use Illuminate\Support\Carbon;
 use Apastorts\JWGetter\Model\Meeting;
+use Apastorts\JWGetter\Services\TalkService;
 
 class MidweekController
 {
@@ -19,9 +20,9 @@ class MidweekController
         $treasures = [];
 
         $treasures['title'] = ['Title',$html->find('#section2')['0']->find('h2')[0]->find('strong')[0]->text];
-        $treasures['talk'] = ['Talk' , self::getTalk($html->find('#section2')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[0]->find('#p6')[0])];
-        $treasures['gems'] = [ 'Gems', self::getTalk($html->find('#section2')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[1]->find('#p10')[0])];
-        $treasures['reading'] = ['Bible Reading', self::getTalk($html->find('#section2')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[2]->find('#p15')[0])];
+        $treasures['talk'] = ['Talk' , TalkService::get($html->find('#section2')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[0]->find('#p6')[0])];
+        $treasures['gems'] = [ 'Gems', TalkService::get($html->find('#section2')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[1]->find('#p10')[0])];
+        $treasures['reading'] = ['Bible Reading', TalkService::get($html->find('#section2')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[2]->find('#p15')[0])];
         $treasures['lesson'] = $html->find('#section2')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[2]->find('#p15')[0]->find('a')[1]->find('em')[0]->text;
         $treasures['lesson'] = ['Lesson to Work', $treasures['lesson'].$html->find('#section2')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[2]->find('#p15')[0]->find('a')[1]->text];
         
@@ -39,12 +40,12 @@ class MidweekController
         $teachers = [];
 
         $teachers['title'] = ['Title',$html->find('#section3')['0']->find('h2')[0]->find('strong')[0]->text];
-        $teachers['video'] = ['Video', self::getTalk($html->find('#section3')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[0]->find('#p17')[0])];
-        $teachers['assign'] = [ 'First Assignment', self::getTalk($html->find('#section3')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[1]->find('#p18')[0])];
-        $teachers['2assign'] = ['Second Assignment', self::getTalk($html->find('#section3')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[2]->find('#p19')[0])];
+        $teachers['video'] = ['Video', TalkService::get($html->find('#section3')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[0]->find('#p17')[0])];
+        $teachers['assign'] = [ 'First Assignment', TalkService::get($html->find('#section3')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[1]->find('#p18')[0])];
+        $teachers['2assign'] = ['Second Assignment', TalkService::get($html->find('#section3')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[2]->find('#p19')[0])];
         
         if($html->find('#section3')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[3]){
-            $teachers['3assign'] = ['Third Assignment', self::getTalk($html->find('#section3')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[3]->find('#p20')[0])];    
+            $teachers['3assign'] = ['Third Assignment', TalkService::get($html->find('#section3')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[3]->find('#p20')[0])];    
         }
 
         return $teachers;
@@ -61,15 +62,15 @@ class MidweekController
         $living = [];
 
         $living['title'] = ['Title',$html->find('#section4')['0']->find('h2')[0]->find('strong')[0]->text];
-        $living['talk'] = ['Talk', self::getTalk($html->find('#section4')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[1]->find('#p22')[0])];
-        $living['second'] = ['Needs Or Book', self::getTalk($html->find('#section4')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[2]->find('#p23')[0])];
+        $living['talk'] = ['Talk', TalkService::get($html->find('#section4')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[1]->find('#p22')[0])];
+        $living['second'] = ['Needs Or Book', TalkService::get($html->find('#section4')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[2]->find('#p23')[0])];
 
         if($html->find('#section4')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[4]){
-            $living['book'] = ['Final or Book', self::getTalk($html->find('#section4')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[3]->find('#p24')[0])];
+            $living['book'] = ['Final or Book', TalkService::get($html->find('#section4')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[3]->find('#p24')[0])];
         }
 
         if($html->find('#section4')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[5]){
-            $living['final'] = ['Final', self::getTalk($html->find('#section4')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[4]->find('#p25')[0])];
+            $living['final'] = ['Final', TalkService::get($html->find('#section4')['0']->find('.pGroup')[0]->find('ul')[0]->find('li')[4]->find('#p25')[0])];
         }
 
         return $living;
@@ -85,103 +86,5 @@ class MidweekController
     public static function saveMeeting(Array $meeting, $date)
     {
         Meeting::firstOrCreate(['date' => $date],['metadata' => json_encode($meeting)]);
-    }
-
-    /**
-     * Takes the talks titles from the living as christian section depending.
-     *
-     * @param Array $talk
-     * @return void
-     */
-    private static function getTalk($talk)
-    {
-        if(($talk->find('strong') && $talk->find('strong')->count() > 0 ) && ($talk->find('a') && $talk->find('a')->count() > 0)){
-            if($talk->text){
-                if($talk->find('strong')[0]->find('em') && $talk->find('strong')[0]->find('em')->count() > 0){
-                    if($talk->find('a')[0]->find('strong') && $talk->find('a')[0]->find('strong')->count() > 0){
-                        return $talk->find('strong')[0]->find('em')[0]->text.$talk->find('a')[0]->find('strong')[0]->text.$talk->text;
-                    }
-                    else{
-                        if($talk->find('a')[0]->find('em') && $talk->find('a')[0]->find('em')->count() > 0){
-                            return $talk->find('strong')[0]->find('em')[0]->text.$talk->find('a')[0]->find('em')[0]->text.$talk->text;
-                        }
-                        else{
-                            return $talk->find('strong')[0]->find('em')[0]->text.$talk->find('a')[0]->text.$talk->text;
-                        }
-                    } 
-                }
-                else{
-                    if($talk->find('a')[0]->find('strong')  && $talk->find('a')[0]->find('strong')->count() > 0){
-                        return $talk->find('strong')[0]->text.$talk->find('a')[0]->find('strong')[0]->text.$talk->text;
-                    }
-                    else{
-                        return $talk->find('strong')[0]->text.$talk->find('a')[0]->text.$talk->text;
-                    } 
-                }  
-            }
-            else{
-                if($talk->find('strong')[0]->find('em') && $talk->find('strong')[0]->find('em')->count() > 0){
-                    if($talk->find('a')[0]->find('strong') && $talk->find('a')[0]->find('strong')->count() > 0){
-                        return $talk->find('strong')[0]->find('em')[0]->text.$talk->find('a')[0]->find('strong')[0]->text;
-                    }
-                    else{
-                        if($talk->find('a')[0]->find('em') && $talk->find('a')[0]->find('em')->count() > 0){
-                            return $talk->find('strong')[0]->find('em')[0]->text.$talk->find('a')[0]->find('em')[0]->text;
-                        }
-                        else{
-                            return $talk->find('strong')[0]->find('em')[0]->text.$talk->find('a')[0]->text;
-                        }
-                    } 
-                }
-                else{
-                    if($talk->find('a')[0]->find('strong')  && $talk->find('a')[0]->find('strong')->count() > 0){
-                        return $talk->find('strong')[0]->text.$talk->find('a')[0]->find('strong')[0]->text;
-                    }
-                    else{
-                        return $talk->find('strong')[0]->text.$talk->find('a')[0]->text;
-                    } 
-                }  
-            }
-        }
-        elseif(!($talk->find('strong') && $talk->find('strong')->count() > 0 ) && !($talk->find('a') && $talk->find('a')->count() > 0)){
-            return $talk->text;
-        }
-        elseif(!($talk->find('strong') && $talk->find('strong')->count() > 0 ))
-        {
-            if($talk->text){
-                if($talk->find('a')[0]->find('strong')[0]->find('em')){
-                    return $talk->find('a')[0]->find('strong')[0]->find('em')[0]->text.$talk->text;
-                }
-                else{
-                    return $talk->find('a')[0]->find('strong')[0]->text.$talk->text;
-                }
-            }
-            else{
-                if($talk->find('a')[0]->find('strong')[0]->find('em')){
-                    return $talk->find('a')[0]->find('strong')[0]->find('em')[0]->text;
-                }
-                else{
-                    return $talk->find('a')[0]->find('strong')[0]->text;
-                }
-            }
-        }
-        elseif(!($talk->find('a') && $talk->find('a')->count() > 0)){
-            if($talk->text){
-                if($talk->find('strong')[0]->find('em') && $talk->find('strong')[0]->find('em')->count() > 0){
-                    return $talk->find('strong')[0]->find('em')[0]->text.$talk->text;
-                }
-                else{
-                    return $talk->find('strong')[0]->text.$talk->text;
-                }
-            }
-            else{
-                if($talk->find('strong')[0]->find('em') && $talk->find('strong')[0]->find('em')->count() > 0){
-                    return $talk->find('strong')[0]->find('em')[0]->text;
-                }
-                else{
-                    return $talk->find('strong')[0]->text;
-                }
-            }
-        }
     }
 }
---
+++ app/Services/TalkService.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Apastorts\JWGetter\Services;
+
+class TalkService
+{
+    public static function get($talk)
+    {
+        return self::organize($talk);
+    }
+
+    public static function organize($talk)
+    {
+        $talk = self::strip($talk);
+
+        $talk = str_replace(': video (', ' (', $talk);
+        $talk = str_replace('the .', 'the video.', $talk);
+
+        $talk = str_replace(': sample conversation (', ' (', $talk);
+        $talk = str_replace('the ,', 'the sample conversation,', $talk);
+        $talk = str_replace('the . ()', 'the sample conversation.', $talk);
+
+        return $talk;
+    }
+
+    /**
+     * Takes the talks titles and descriptions from the midweek meeting html.
+     *
+     * @param Array $talk
+     * @return void
+     */
+    public static function strip($talk)
+    {
+        if(($talk->find('strong') && $talk->find('strong')->count() > 0 ) && ($talk->find('a') && $talk->find('a')->count() > 0)){
+            if($talk->text){
+                if($talk->find('strong')[0]->find('em') && $talk->find('strong')[0]->find('em')->count() > 0){
+                    if($talk->find('a')[0]->find('strong') && $talk->find('a')[0]->find('strong')->count() > 0){
+                        return $talk->find('strong')[0]->find('em')[0]->text.$talk->find('a')[0]->find('strong')[0]->text.$talk->text;
+                    }
+                    else{
+                        if($talk->find('a')[0]->find('em') && $talk->find('a')[0]->find('em')->count() > 0){
+                            return $talk->find('strong')[0]->find('em')[0]->text.$talk->find('a')[0]->find('em')[0]->text.$talk->text;
+                        }
+                        else{
+                            return $talk->find('strong')[0]->find('em')[0]->text.$talk->find('a')[0]->text.$talk->text;
+                        }
+                    } 
+                }
+                else{
+                    if($talk->find('a')[0]->find('strong')  && $talk->find('a')[0]->find('strong')->count() > 0){
+                        return $talk->find('strong')[0]->text.$talk->find('a')[0]->find('strong')[0]->text.$talk->text;
+                    }
+                    else{
+                        return $talk->find('strong')[0]->text.$talk->find('a')[0]->text.$talk->text;
+                    } 
+                }  
+            }
+            else{
+                if($talk->find('strong')[0]->find('em') && $talk->find('strong')[0]->find('em')->count() > 0){
+                    if($talk->find('a')[0]->find('strong') && $talk->find('a')[0]->find('strong')->count() > 0){
+                        return $talk->find('strong')[0]->find('em')[0]->text.$talk->find('a')[0]->find('strong')[0]->text;
+                    }
+                    else{
+                        if($talk->find('a')[0]->find('em') && $talk->find('a')[0]->find('em')->count() > 0){
+                            return $talk->find('strong')[0]->find('em')[0]->text.$talk->find('a')[0]->find('em')[0]->text;
+                        }
+                        else{
+                            return $talk->find('strong')[0]->find('em')[0]->text.$talk->find('a')[0]->text;
+                        }
+                    } 
+                }
+                else{
+                    if($talk->find('a')[0]->find('strong')  && $talk->find('a')[0]->find('strong')->count() > 0){
+                        return $talk->find('strong')[0]->text.$talk->find('a')[0]->find('strong')[0]->text;
+                    }
+                    else{
+                        return $talk->find('strong')[0]->text.$talk->find('a')[0]->text;
+                    } 
+                }  
+            }
+        }
+        elseif(!($talk->find('strong') && $talk->find('strong')->count() > 0 ) && !($talk->find('a') && $talk->find('a')->count() > 0)){
+            return $talk->text;
+        }
+        elseif(!($talk->find('strong') && $talk->find('strong')->count() > 0 ))
+        {
+            if($talk->text){
+                if($talk->find('a')[0]->find('strong')[0]->find('em')){
+                    return $talk->find('a')[0]->find('strong')[0]->find('em')[0]->text.$talk->text;
+                }
+                else{
+                    return $talk->find('a')[0]->find('strong')[0]->text.$talk->text;
+                }
+            }
+            else{
+                if($talk->find('a')[0]->find('strong')[0]->find('em')){
+                    return $talk->find('a')[0]->find('strong')[0]->find('em')[0]->text;
+                }
+                else{
+                    return $talk->find('a')[0]->find('strong')[0]->text;
+                }
+            }
+        }
+        elseif(!($talk->find('a') && $talk->find('a')->count() > 0)){
+            if($talk->text){
+                if($talk->find('strong')[0]->find('em') && $talk->find('strong')[0]->find('em')->count() > 0){
+                    return $talk->find('strong')[0]->find('em')[0]->text.$talk->text;
+                }
+                else{
+                    return $talk->find('strong')[0]->text.$talk->text;
+                }
+            }
+            else{
+                if($talk->find('strong')[0]->find('em') && $talk->find('strong')[0]->find('em')->count() > 0){
+                    return $talk->find('strong')[0]->find('em')[0]->text;
+                }
+                else{
+                    return $talk->find('strong')[0]->text;
+                }
+            }
+        }
+    }
+}
